2e95243cdcbe9610d4a009a078764bf85a8d805b
Nataliia Nazarko <90257127+Nataliia-Nazarko@users.noreply.github.com>
2024-02-24 15:12:37 +0100

    components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,11 @@
-// src/App.jsx
+// src/components/App.jsx
 
-const Product = () => {
-  return (
-    <div>
-      <h2>Cookies</h2>
-			<p>Price: 999 credits</p>
-    </div>
-  );
-};
+import Product from './Product';
 
 export default function App() {
   return (
     <div>
-      <h1>Products</h1>
+      <h1>Products cbdfhd</h1>
 
       <Product />
       <Product />
